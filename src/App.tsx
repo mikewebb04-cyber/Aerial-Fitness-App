fe73cb86ba0655c6956a30aa50c4d5291caae7f0
mikewebb04-cyber <mikewebb04@gmail.com>
2026-04-23 12:46:28 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: "20px", fontFamily: "sans-serif" }}>
+      <h1>My Aerial Fitness Journey</h1>
+      <p>This app will track my pole, hoop and aerial progress.</p>
+    </div>
+  );
+}
+
+export default App;
